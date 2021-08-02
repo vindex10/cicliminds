1ba528a8bf6a4ec12c7b1c26c7e6e65f0f318dbe
vindex10 <vindex10@gmail.com>
2021-08-02 19:01:57 +0000

    add rebuild new button. fix ordering critical bug

--- cicliminds/widgets/staged.py
+++ cicliminds/widgets/staged.py
@@ -7,13 +7,16 @@ class StagedWidget(ObserverWidget):
     def __init__(self):
         self.state = {}
         self.state["button_rebuild_all"] = self._get_rebuild_all_button()
+        self.state["button_build_new"] = self._get_build_new_button()
         self.state["button_unstage_all"] = self._get_button_unstage_all()
         self.state["staged_list"] = VBox()
         self._block_widgets = []
         super().__init__()
 
     def render(self):
-        staged_controls = HBox([self.state["button_rebuild_all"], self.state["button_unstage_all"]])
+        staged_controls = HBox([self.state["button_rebuild_all"],
+                                self.state["button_build_new"],
+                                self.state["button_unstage_all"]])
         staged_widget = VBox([
             Label("Staged for plotting:"),
             staged_controls,
@@ -28,7 +31,7 @@ class StagedWidget(ObserverWidget):
             new_block.observe(self.propagate)
             rendered_blocks.append(new_block.render())
             self._block_widgets.insert(0, new_block)
-        self.state["staged_list"].children = tuple(rendered_blocks) + self.state["staged_list"].children
+        self.state["staged_list"].children = tuple(rendered_blocks[::-1]) + self.state["staged_list"].children
 
     def get_state(self):
         res = []
@@ -41,6 +44,11 @@ class StagedWidget(ObserverWidget):
         button_rebuild_all = Button(description="Rebuild all", icon="redo", button_style="success")
         button_rebuild_all.on_click(self._rebuild_all_action)
         return button_rebuild_all
+
+    def _get_build_new_button(self):
+        button_build_new = Button(description="Build new", icon="redo", button_style="info")
+        button_build_new.on_click(self._build_new_action)
+        return button_build_new
 
     def _get_button_unstage_all(self):
         button_unstage_all = Button(description="Unstage all", icon="trash", button_style="danger")
@@ -61,6 +69,12 @@ class StagedWidget(ObserverWidget):
 
     def _rebuild_all_action(self, change):  # pylint: disable=unused-argument
         for block in self._block_widgets:
+            block.state["rebuild_button"].click()
+
+    def _build_new_action(self, change):  # pylint: disable=unused-argument
+        for block in self._block_widgets:
+            if block.state["output"].outputs:
+                continue
             block.state["rebuild_button"].click()
 
     def _unstage_all_action(self, change):  # pylint: disable=unused-argument

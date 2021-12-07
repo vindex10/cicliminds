0ca1039048629fd494262415ab59849df3da34a3
vindex10 <vindex10@gmail.com>
2021-12-07 19:52:38 +0000

    remove unsupported plot type

--- cicliminds/interface/plot_types.py
+++ cicliminds/interface/plot_types.py
@@ -1,14 +1,11 @@
 from cicliminds_lib.plotting.recipes.means_of_hists import MeansOfHistsRecipe
 from cicliminds_lib.plotting.recipes.means_of_hists import MeansOfHistsDiffRecipe
-from cicliminds_lib.plotting.recipes.hists_of_means import HistsOfMeansRecipe
-from cicliminds_lib.plotting.recipes.hists_of_means import HistsOfMeansDiffRecipe
 from cicliminds_lib.plotting.recipes.mean_val import MeanValRecipe
 from cicliminds_lib.plotting.recipes.mean_val import MeanValDiffRecipe
 from cicliminds_lib.plotting.recipes.time_series import TimeSeriesRecipe
 from cicliminds_lib.plotting.recipes.time_series import TimeSeriesDiffRecipe
 
 PLOT_TYPES_SPEC = {
-    "fldmean first": [HistsOfMeansRecipe, HistsOfMeansDiffRecipe],
     "fldmean last": [MeansOfHistsRecipe, MeansOfHistsDiffRecipe],
     "mean val": [MeanValRecipe, MeanValDiffRecipe],
     "time series": [TimeSeriesRecipe, TimeSeriesDiffRecipe]
